fb540a261a8623aebab8cba1847c1b094f04d075
AlexandFunduk <kriptograff56@gmail.com>
2020-10-31 16:41:38 +0300

    implement CrudMealRepository and create profiles in spring-db

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,26 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    Meal getMealByIdAndUser(Integer integer, User user);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Meal meal WHERE meal.id =:id AND meal.user.id=:user_id")
+    int delete(@Param("id") int id, @Param("user_id") int userId);
+
+    List<Meal> findAllByUserAndDateTimeGreaterThanEqualAndDateTimeLessThanOrderByDateTimeDesc(User user, LocalDateTime start, LocalDateTime end);
+
+    List<Meal> findAllByUserOrderByDateTimeDesc(User user);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -2,42 +2,50 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public class DataJpaMealRepository implements MealRepository {
+    private final EntityManager em;
 
     private final CrudMealRepository crudRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
+    public DataJpaMealRepository(EntityManager em, CrudMealRepository crudRepository) {
+        this.em = em;
         this.crudRepository = crudRepository;
     }
 
     @Override
     public Meal save(Meal meal, int userId) {
+        meal.setUser(em.getReference(User.class, userId));
+        if (meal.isNew() || get(meal.id(), userId) != null) {
+            return crudRepository.save(meal);
+        }
         return null;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudRepository.getMealByIdAndUser(id, em.getReference(User.class, userId));
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.findAllByUserOrderByDateTimeDesc(em.getReference(User.class, userId));
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudRepository.findAllByUserAndDateTimeGreaterThanEqualAndDateTimeLessThanOrderByDateTimeDesc(em.getReference(User.class, userId), startDateTime, endDateTime);
     }
 }
