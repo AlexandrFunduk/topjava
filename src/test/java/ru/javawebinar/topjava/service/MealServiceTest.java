cf90313018673c668461ed6c82ed83f161cf48f5
AlexandFunduk <kriptograff56@gmail.com>
2020-10-31 17:31:34 +0300

    create tests for DataJpa, Jpa, JDBC

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -29,7 +29,7 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
-public class MealServiceTest extends AbstractServiceTest {
+public abstract class MealServiceTest extends AbstractServiceTest {
 
     @Autowired
     private MealService service;
