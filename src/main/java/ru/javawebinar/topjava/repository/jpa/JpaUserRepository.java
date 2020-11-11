d4cc4e8aac3e612f1df826bae72fb43fc689db93
AlexandFunduk <kriptograff56@gmail.com>
2020-11-11 13:04:26 +0300

    fix JpaUserRepository for 2 role user

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaUserRepository.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.repository.jpa;
 
-import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.User;
@@ -9,6 +8,7 @@ import ru.javawebinar.topjava.repository.UserRepository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
@@ -62,7 +62,8 @@ public class JpaUserRepository implements UserRepository {
         List<User> users = em.createNamedQuery(User.BY_EMAIL, User.class)
                 .setParameter(1, email)
                 .getResultList();
-        return DataAccessUtils.singleResult(users);
+        Optional<User> user = users.stream().reduce((user1, user2) -> {user1.getRoles().addAll(user2.getRoles()); return user1;});
+        return user.orElse(null);
     }
 
     @Override
