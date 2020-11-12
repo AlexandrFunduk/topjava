55d5677d6e265cdb4aef68f65cc8636e8f7d8bc6
AlexandFunduk <kriptograff56@gmail.com>
2020-11-12 14:13:48 +0300

    Use NoOpCacheManager in test

--- src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
@@ -4,7 +4,8 @@ import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.service.AbstractCacheableUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JPA;
+import static ru.javawebinar.topjava.Profiles.TEST;
 
-@ActiveProfiles(JPA)
+@ActiveProfiles({JPA, TEST})
 public class JpaUserServiceTest extends AbstractCacheableUserServiceTest {
 }
